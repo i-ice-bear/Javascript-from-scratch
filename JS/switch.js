47025d77f2056ab4c7d4f3f7a803fcd6053b9357
ice-bear <andydowsen47us@gmail.com>
2022-08-21 13:47:27 +0530

    Switch fall through commit

--- JS/switch.js
+++ JS/switch.js
@@ -1,3 +1,7 @@
+const animalsList = ["cat", "dog", "hamster", "Dinosaur"];
+
+//+ Basic switch - 1
+
 function checkList() {
   const switchValue = prompt("Enter your switch value : ");
 
@@ -12,6 +16,24 @@ function checkList() {
       alert("Current price of dragon fruit is 20$");
     default:
       alert("We can't find the fruit name, Add a proper fruit!");
+      break;
+  }
+}
+
+//+ Fall through advantage of case switch.
+
+function fallThrough() {
+  const fallThroughes = prompt("Enter the animal name : ");
+  switch (fallThroughes,animalsList.toString()) {
+    case animalsList.toString():
+    case animalsList.toString():
+    case animalsList.toString():
+      alert("not extinct");
+      break;
+    case animalsList.toString():
+      alert("Completely extinct");
+    default:
+      alert("Value not provided, Try again!");
       break;
   }
 }
