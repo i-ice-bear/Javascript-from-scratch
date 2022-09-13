2f0d09e0b9fac1ead4685f298bcc34e5b3dc3561
ice-bear <andydowsen47us@gmail.com>
2022-09-13 20:31:05 +0530

    Function commit

--- JS/functions.js
+++ JS/functions.js
@@ -1,8 +1,18 @@
 let a = 1;
 let b = 2;
 let c = 3;
+
 function sumProperty(){
     let d = a + b + c
     console.log(d / 2)
 }
+
+function takeSomething(){
+    let d = 30
+    let e = 30
+    let f = 20
+    console.log(d + e + f)
+}
+takeSomething()
+
 sumProperty()
