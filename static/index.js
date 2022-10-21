42c6a0fbf39ec358b3cbc8e452623369dab1231f
ice-bear <andydowsen47us@gmail.com>
2022-10-21 16:04:26 +0530

    Dom html content manipulations in /static/index.js #1

--- static/index.js
+++ static/index.js
@@ -1,1 +1,25 @@
 console.log("Execute on the main host")
+
+function alertWindow() {
+    alert("You are using alert on webpage")
+}
+function promptValue(){
+    const a = prompt("Enter you want to feed init : ")
+    const displayerHandler = document.getElementById("contentManage").innerHTML = a;
+    setTimeout(() => {
+        const deleteBack = document.getElementById("contentManage").innerHTML = ""
+        console.table(deleteBack, displayerHandler, "Back to normal")
+    }, 9000);
+}
+function confirmationFunction(){
+    const confirmation = confirm("Do you want to go outside : ");
+    if (confirmation == true){
+        const wherePrompt = prompt("Where you want to go : ")
+        window.open(`https://${wherePrompt}.com`)   
+    }
+    else if(confirmation == false){
+        alert("You are not going anywhere")
+    }else{
+        alert("none")
+    }
+}
